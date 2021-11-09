02dc71c22e3f3fe6a84f3815f360f21a047f351d
iona <iona.arregui@gmail.com>
2021-11-09 17:47:47 -0300

    Agregar carga dinamica de productos

--- app.js
+++ app.js
@@ -4,29 +4,33 @@ const productos = require("./public/products.js");
 let preciofinal = 0;
 
 const producto = require("./public/product.js");
+const otrosproductos = require("./public/otros-productos.js");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => res.render("pages/index", { title: "Home" }));
+app.get("/", (req, res) =>
+  res.render("pages/index", { title: "Home", otrosproductos: otrosproductos })
+);
 
 app.get("/product/:id(\\d+)", (req, res) =>
-    res.render("pages/product", {
-        title: `Producto ${req.params.id}`,
-        producto: producto,
-    })
+  res.render("pages/product", {
+    title: `Producto ${req.params.id}`,
+    producto: producto,
+    otrosproductos: otrosproductos,
+  })
 );
 
 app.get("/cart", (req, res) =>
-    res.render("pages/cart", { productos, preciofinal, title: "Carrito" })
+  res.render("pages/cart", { productos, preciofinal, title: "Carrito" })
 );
 
 app.get("/checkout", (req, res) =>
-    res.render("pages/checkout", { title: "Pago" })
+  res.render("pages/checkout", { title: "Pago" })
 );
 
 app.get("/register", (req, res) =>
-    res.render("pages/register", { title: "Registro" })
+  res.render("pages/register", { title: "Registro" })
 );
 
 app.get("/login", (req, res) => res.render("pages/login", { title: "Login" }));
---
+++ public/otros-productos.js
@@ -0,0 +1,32 @@
+const productos = [
+  {
+    id: 1,
+    categoria: "Viajes",
+    title: "Voucher de $400 en Plataforma 10",
+    price: "11.500",
+    img: "plataforma101.jpg",
+  },
+  {
+    id: 2,
+    categoria: "Alimentos",
+    title: "Alfajores Havanna Chocolate 12 un.",
+    price: "19.900",
+    img: "havanna1.jpg",
+  },
+  {
+    id: 3,
+    categoria: "Alimentos",
+    title: "1 Combo Mega Cuarto Deluxe en Mostaza",
+    price: "10.000",
+    img: "mostaza1.jpg",
+  },
+  {
+    id: 1,
+    categoria: "Alimentos",
+    title: "Voucher 2x1 en Kilo de helado Freddo",
+    price: "11.500",
+    img: "freddo1.jpg",
+  },
+];
+
+module.exports = productos;
