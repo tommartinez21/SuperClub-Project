3a772a9ae7b9638c04910fffccd545ccaac6755a
Kajachuan <kevincajachuan@hotmail.com>
2021-11-15 15:11:25 -0300

    Agrego users.json

--- src/app.js
+++ src/app.js
@@ -1,34 +1,51 @@
 const express = require("express");
-const loginRoutes = require('./routes/login');
-const registerRoutes = require('./routes/register');
-const cartRoutes = require('./routes/cart');
-const productRoutes = require('./routes/product');
-const checkoutRoutes = require('./routes/checkout');
-const indexRoutes = require('./routes/index');
+const session = require("express-session");
+const loginRoutes = require("./routes/login");
+const registerRoutes = require("./routes/register");
+const cartRoutes = require("./routes/cart");
+const productRoutes = require("./routes/product");
+const checkoutRoutes = require("./routes/checkout");
+const indexRoutes = require("./routes/index");
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 4000;
+
 app.set("view engine", "ejs");
-app.set('views', 'src/views')
+app.set("views", "src/views");
 
 //routes
 
 app.use(express.static("public"));
 
-app.use('/', indexRoutes)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use('/register', registerRoutes)
-
-app.use('/login', loginRoutes)
-
-app.use('/checkout', checkoutRoutes)
-
-app.use('/cart', cartRoutes)
-
-app.use('/product', productRoutes)
-
-app.get("*", (req, res) =>
-    res.status(404).render("pages/error", { title: "404 !" })
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
 );
 
-app.listen(3000, () => console.log("toy ready"));
+app.use("/", indexRoutes);
+
+app.use("/register", registerRoutes);
+
+app.use("/login", loginRoutes);
+
+app.use("/checkout", checkoutRoutes);
+
+app.use("/cart", cartRoutes);
+
+app.use("/product", productRoutes);
+
+app.get("*", (req, res) =>
+  res.status(404).render("pages/error", { title: "404 !" })
+);
+
+app.listen(PORT, () => console.log("toy ready"));
