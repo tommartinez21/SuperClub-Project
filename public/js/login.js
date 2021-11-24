1fbcd46ebd26ee732263cd4b51812b62f2550800
Kajachuan <kevincajachuan@hotmail.com>
2021-11-24 18:25:39 -0300

    Cambio de tema

--- public/js/login.js
+++ public/js/login.js
@@ -1,19 +1,18 @@
 let rememberCheck = document.querySelector("#remember-email");
 let email = document.querySelector("#email");
-let password = document.querySelector("#password")
+let password = document.querySelector("#password");
 let loginForm = document.querySelector(".formulario");
 let divErrores = document.querySelector("#div-errores");
 let modalLogin = document.querySelector("#modalLogin");
-let loginBtn = document.querySelector("#loginBtn")
-let errPassw = true
-let errEmail = true
+let loginBtn = document.querySelector("#loginBtn");
+let errPassw = true;
+let errEmail = true;
 
 const toggleButton = () => {
-  console.log("boton disabled? ", loginBtn.disabled)
-  console.log("errEmail: " + errEmail + ", errPassw: " + errPassw)
-  loginBtn.disabled = errEmail || errPassw ? true : false
-
-}
+  console.log("boton disabled? ", loginBtn.disabled);
+  console.log("errEmail: " + errEmail + ", errPassw: " + errPassw);
+  loginBtn.disabled = errEmail || errPassw ? true : false;
+};
 
 window.addEventListener("load", (e) => {
   let emailStorage = localStorage.getItem("email");
@@ -21,11 +20,15 @@ window.addEventListener("load", (e) => {
   if (emailStorage) {
     email.value = emailStorage;
     rememberCheck.checked = true;
-    errEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email.value) ? false : true
-    toggleButton()
+    errEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(
+      email.value
+    )
+      ? false
+      : true;
+    toggleButton();
   }
 
-  if (divErrores && divErrores.innerHTML !== ""){
+  if (divErrores && divErrores.innerHTML !== "") {
     e.preventDefault();
     modalLogin.hidden = false;
   }
@@ -40,16 +43,20 @@ loginForm.addEventListener("submit", (e) => {
 });
 
 email.addEventListener("input", (e) => {
-  errEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(e.target.value) ? false : true
-  toggleButton()
-})
+  errEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(
+    e.target.value
+  )
+    ? false
+    : true;
+  toggleButton();
+});
 
 password.addEventListener("keyup", (e) => {
-  errPassw = e.target.value.length < 8 ? true : false
-  toggleButton()
-})
+  errPassw = e.target.value.length < 8 ? true : false;
+  toggleButton();
+});
 
 closeModal = () => {
-  console.log()
-  modalLogin.hidden=true
-}
+  console.log();
+  modalLogin.hidden = true;
+};
