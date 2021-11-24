69f4004b6f8cf1d5592485d28af744d3234fe8d4
Nicolas Sandobal <nicsandobal@gmail.com>
2021-11-24 10:33:29 -0300

    Boton de registro con disabling

--- public/js/validacionRegistro.js
+++ public/js/validacionRegistro.js
@@ -2,45 +2,74 @@ let formulario = document.querySelector("#formulario");
 let emailUsuario = document.querySelector("#emailUsuario");
 let nombreUsuario = document.querySelector("#nombreUsuario");
 let contraseñaUsuario = document.querySelector("#contraseñaUsuario");
+
 let errorPassword = document.querySelector(".errorPassword");
 let errorEmail = document.querySelector(".errorEmail");
 let errorNombreUsuario = document.querySelector(".errorNombreUsuario");
+
+let registerBtn = document.querySelector("#registerBtn")
+let errPassw = true
+let errEmail = true
+let errName = true
+
+const toggleButton = () => {
+    console.log("yendooo", errEmail, errName, errPassw)
+    if (errPassw || errEmail || errName) {
+        // si todos están "hidden" significa que no hay errores
+        registerBtn.disabled = true
+    } else {
+        registerBtn.disabled = false
+    }
+}
 
 contraseñaUsuario.addEventListener("keyup", (e) =>{
     if (e.target.value.length <8){
         errorPassword.innerHTML = "Tiene que tener más de 8 caracteres";
         errorPassword.hidden = false;
+        errPassw = true
     }
     else{
         errorPassword.innerHTML = "Contraseña válida"
+        errPassw = false
     }
+    toggleButton()
 })
 
 nombreUsuario.addEventListener("focus", (e) => {
     if (e.target.value === ""){
         errorNombreUsuario.innerHTML = "No puede estar en blanco";
         errorNombreUsuario.hidden = false;
+        errName = true
+    } else {
+        errName = false
     }
+    toggleButton()
 })
 
 nombreUsuario.addEventListener("keyup", (e) => {
     if (e.target.value !== ""){
         errorNombreUsuario.innerHTML = "Válido";
+        errName = false
     }
     else{
         errorNombreUsuario.innerHTML = "No puede estar en blanco";
+        errName = true
     }
+    toggleButton()
 })
 
 emailUsuario.addEventListener("input", (e) => {
     const emailInputValue = e.target.value;
-
+    console.log(emailInputValue)
+    console.log(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(emailInputValue))
     if (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(emailInputValue) != true){
         errorEmail.innerHTML = "Email inválido";
         errorEmail.hidden = false;
-    }
-    else {
+        errEmail = true
+    } else {
         errorEmail.innerHTML = "Email válido";
+        errEmail = false
     }
+    toggleButton()
 })
 
