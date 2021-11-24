7d5067391afba220c8cb4f2b405ae819cae0e221
SolMansilla <mariasmansilla@santandertecnologia.com.ar>
2021-11-24 12:21:06 -0300

    modal del login

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,18 @@
 let rememberCheck = document.querySelector("#remember-email");
 let email = document.querySelector("#email");
 let loginForm = document.querySelector(".formulario");
+let divErrores = document.querySelector("#div-errores");
+let modalLogin = document.querySelector("#modalLogin");
 
 window.addEventListener("load", (e) => {
   let emailStorage = localStorage.getItem("email");
   if (emailStorage) {
     email.value = emailStorage;
     rememberCheck.checked = true;
+  }
+  if (divErrores.innerHTML !== ""){
+    e.preventDefault();
+    modalLogin.hidden = false;
   }
 });
 
@@ -17,3 +23,9 @@ loginForm.addEventListener("submit", (e) => {
     localStorage.removeItem("email");
   }
 });
+
+
+closeModal = () => {
+  console.log()
+  modalLogin.hidden=true
+}
