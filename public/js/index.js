d87bff29004599d43b7c2d1a14752da58806f95e
tommartinez21 <tommartinez@santandertecnologia.com.ar>
2021-11-24 15:06:52 -0300

    User Story 2

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,18 @@
 const userButton = document.querySelector('.user-button');
-const menu = document.querySelector('.menu')
-const clickOut = document.querySelector('.clickOutMenu')
-const html = document.querySelector('html')
-
+const menu = document.querySelector('.menu');
+const clickOut = document.querySelector('.clickOutMenu');
+const html = document.querySelector('html');
+const changeMode = document.querySelector('.changeMode');
+ 
 const onClickUserButton = () => {
     clickOut.classList.toggle('abiertoClickOut')
     screen.width > 1072? menu.classList.toggle('abiertoD') : menu.classList.toggle('abiertoM')
+    clickOut.classList.contains('abiertoClickOut')? html.style.overflow='hidden' : html.style.removeProperty('overflow')
 };
 
-const onClickFuera = (e) => {
+const onClickFuera = () => {
         clickOut.classList.remove('abiertoClickOut')
+        html.style.removeProperty('overflow')
         screen.width > 1072? menu.classList.remove('abiertoD') : menu.classList.remove('abiertoM')
     
 };
@@ -18,3 +21,5 @@ const onClickFuera = (e) => {
 userButton.addEventListener('click', onClickUserButton)
 
 clickOut.addEventListener('click', onClickFuera)
+
+changeMode.addEventListener('click', modoOscuro)
