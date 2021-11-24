065f5dc7c579c5041154656c8c75dbf308d8e1df
tommartinez21 <tommartinez@santandertecnologia.com.ar>
2021-11-24 16:43:38 -0300

    activar modo OSCURO

--- public/js/darkMode.js
+++ public/js/darkMode.js
@@ -1,8 +1,12 @@
 let modoOscuro = () => {
   let skins = document.querySelectorAll(".skin");
+  let botonSwitch = document.querySelector('#botonSwitch')
+  let opcionSwitch = document.querySelector('#opcionSwitch')
   skins.forEach((e) => {
     e.classList.toggle("dark");
   });
   document.querySelector("body").classList.toggle("dark");
   document.querySelector("footer").classList.toggle("dark");
+  botonSwitch.classList.toggle('botonSwitchOn')
+  opcionSwitch.classList.toggle('opcionSwitchOn')
 };
