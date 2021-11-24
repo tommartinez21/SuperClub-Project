ca08dd023d7ff8af0d1abcc425074d90ab8db7b3
Kajachuan <kevincajachuan@hotmail.com>
2021-11-24 13:19:33 -0300

    Carrito andando

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -5,42 +5,38 @@ let preciofinal = 0;
 
 const cartController = {
   renderCart(req, res) {
+    req.session.previousUrl = req.originalUrl;
     if (!req.session.user) {
       console.error("El usuario no está logeado");
       res.redirect("/login");
+    } else {
+      let productsInCart = [];
+      fetch("http://dhfakestore.herokuapp.com/api/products")
+        .then((res) => res.json())
+        .then((data) => {
+          req.session.user.cart.forEach((product) => {
+            let productFromApi = data.find(
+              (element) => element._id === product.id
+            );
+            productFromApi.cantidad = product.cantidad;
+            productsInCart.push(productFromApi);
+          });
+          return productsInCart;
+        })
+        .then((productsInCart) => {
+          let productos = productsInCart;
+          res.render("pages/cart", {
+            productos,
+            preciofinal,
+            title: "Carrito",
+            session: req.session,
+          });
+        })
+        .catch((err) => {
+          res.send("ERROR, VER CONSOLA");
+          console.error(err);
+        });
     }
-    let idUser = req.session.user.id;
-    let userLogged = users.find((usuario) => usuario.id === idUser);
-
-    req.session.previousUrl = req.originalUrl;
-    
-
-    let productsInCart = [];
-    fetch("http://dhfakestore.herokuapp.com/api/products")
-      .then((res) => res.json())
-      .then((data) => {
-        userLogged.cart.forEach((product) => {
-          let productFromApi = data.find(
-            (element) => element._id === product.id
-          );
-          productFromApi.cantidad = product.cantidad;
-          productsInCart.push(productFromApi);
-        });
-        return productsInCart;
-      })
-      .then((productsInCart) => {
-        let productos = productsInCart;
-        res.render("pages/cart", {
-          productos,
-          preciofinal,
-          title: "Carrito",
-          session: req.session,
-        });
-      })
-      .catch((err) => {
-        res.send("ERROR, VER CONSOLA");
-        console.error(err);
-      });
   },
 };
 
