f580dbb85bb97039282ecf38a344bec4b8183806
tommartinez21 <tommartinez@santandertecnologia.com.ar>
2021-11-25 11:44:45 -0300

    union con correccion de tomi

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,25 @@
-const userButton = document.querySelector(".user-button");
-const menu = document.querySelector(".menu");
-const clickOut = document.querySelector(".clickOutMenu");
-const html = document.querySelector("html");
-const changeMode = document.querySelector(".changeMode");
-
+const userButton = document.querySelector('.user-button');
+const menu = document.querySelector('.menu');
+const clickOut = document.querySelector('.clickOutMenu');
+const html = document.querySelector('html');
+const changeMode = document.querySelector('.changeMode');
+ 
 const onClickUserButton = () => {
-  clickOut.classList.toggle("abiertoClickOut");
-  screen.width > 1072
-    ? menu.classList.toggle("abiertoD")
-    : menu.classList.toggle("abiertoM");
-  clickOut.classList.contains("abiertoClickOut")
-    ? (html.style.overflow = "hidden")
-    : html.style.removeProperty("overflow");
+    clickOut.classList.toggle('abiertoClickOut')
+    screen.width > 1072? menu.classList.toggle('abiertoD') : menu.classList.toggle('abiertoM')
+    clickOut.classList.contains('abiertoClickOut')? html.style.overflow='hidden' : html.style.removeProperty('overflow')
 };
 
 const onClickFuera = () => {
-  clickOut.classList.remove("abiertoClickOut");
-  html.style.removeProperty("overflow");
-  screen.width > 1072
-    ? menu.classList.remove("abiertoD")
-    : menu.classList.remove("abiertoM");
+        clickOut.classList.remove('abiertoClickOut')
+        html.style.removeProperty('overflow')
+        screen.width > 1072? menu.classList.remove('abiertoD') : menu.classList.remove('abiertoM')
+    
 };
 
-userButton.addEventListener("click", onClickUserButton);
 
-clickOut.addEventListener("click", onClickFuera);
+userButton.addEventListener('click', onClickUserButton)
 
-changeMode.addEventListener("click", cambiarTema);
+clickOut.addEventListener('click', onClickFuera)
+
+changeMode.addEventListener('click', modoOscuro)
